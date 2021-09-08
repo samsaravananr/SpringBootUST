8343a1a8d7a496fc632006808a1912b8acb066ff
samsaravananr <samsaravananr@gmail.com>
2021-09-08 10:26:21 +0530

    This is Third Commit - with JUNIT

---
+++ EMS_UST/src/main/java/com/example/EMS_UST/GetAndSet.java
@@ -0,0 +1,20 @@
+package com.example.EMS_UST;
+
+public class GetAndSet 
+{
+	
+	
+	private String color;
+
+	public String getColor() 
+	{
+		return color;
+	}
+
+	public void setColor(String color) 
+	{
+		this.color = color;
+	}
+	
+
+}
--- EMS_UST/src/test/java/com/example/EMS_UST/EmsUstApplicationTests.java
+++ EMS_UST/src/test/java/com/example/EMS_UST/EmsUstApplicationTests.java
@@ -1,15 +1,38 @@
 package com.example.EMS_UST;
 
+
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmsUstApplicationTests {
+class EmsUstApplicationTests 
+{
 
-	@Test
+	
 	void contextLoads() 
 	{
-		System.out.println("Test");
+		
+			System.out.println("Hello World");
+		
 	}
+	
+	
+	@Test
+	public void TS001()
+	{
+		GetAndSet GS=new GetAndSet();
+		GS.setColor("Red");
+		System.out.println(GS.getColor());
+		assertEquals("Yellow", GS.getColor());
+		
+	}
+	
+	
+	
+	
+	
 
 }
