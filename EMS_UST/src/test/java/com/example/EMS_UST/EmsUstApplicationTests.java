4a8df88276f94a6341c3d991d295f155256cf00f
samsaravananr <samsaravananr@gmail.com>
2021-09-08 10:59:18 +0530

    FIFITH Commit - Adding JUNIT Test

--- EMS_UST/src/test/java/com/example/EMS_UST/EmsUstApplicationTests.java
+++ EMS_UST/src/test/java/com/example/EMS_UST/EmsUstApplicationTests.java
@@ -5,11 +5,19 @@ package com.example.EMS_UST;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class EmsUstApplicationTests 
 {
+	
+	
+	@Autowired 
+	EMPUSTController con;
+	
 
 	
 	void contextLoads() 
@@ -20,7 +28,7 @@ class EmsUstApplicationTests
 	}
 	
 	
-	@Test
+	//@Test
 	public void TS001()
 	{
 		GetAndSet GS=new GetAndSet();
@@ -31,6 +39,28 @@ class EmsUstApplicationTests
 	}
 	
 	
+	@Test
+	public void TS002()
+	{
+		EMS ems=EMSDATA();
+		
+		ResponseEntity response=con.addEMP(ems);
+		System.out.println(response.getStatusCode());
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+	}
+	
+	
+	public EMS EMSDATA()
+	{
+		EMS ems=new EMS();
+		//ems1.seteID("Azees1");
+		ems.seteNAME("Aishwarya");
+		ems.seteSAL("50000");
+		return ems;
+	}
+	
+	
+	// Mockito ---> Framework for mocking external dependencies 
 	
 	
 	
